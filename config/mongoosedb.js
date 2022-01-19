20155a943fd365a538eed2b2d08bd9b5ccc8bc8e
Anonymous-alpha-linux <tinhntgcd18753@fpt.edu.vn>
2022-01-19 21:32:45 +0700

    modify app with secrete

--- config/mongoosedb.js
+++ config/mongoosedb.js
@@ -1,34 +1,10 @@
 // const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
-// const MongooseClient = new MongoClient(process.env.MONGOOSE_URI);
-
-// class ContextDb {
-//     constructor() {
-//     }
-//     connect(callback) {
-//         MongooseClient.connect()
-//             .then(result => {
-//                 console.log('Connected to MongoDb server');
-//                 this._db = result.db();
-//                 if (callback instanceof Function) callback(this_db);
-//             }).catch(err => {
-//                 console.log(err.stack);
-//             }).finally(() => {
-//                 MongooseClient.close();
-//             })
-//     }
-//     getDb() {
-//         if (this._db) {
-//             return this._db;
-//         }
-//         throw new Error('There are no database');
-//         // console.log("error on database");
-//     }
-// }
 
 function connectToMongoDB(callback) {
     // let uri = process.env.MONGOOSE_URI;
-    let uri = "mongodb://127.0.0.1:27017/test";
+    let uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@e-commerce.zf1wu.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
+    // let uri = "mongodb://127.0.0.1:27017/test";
     return mongoose
         .connect(uri)
         .then(client => {
